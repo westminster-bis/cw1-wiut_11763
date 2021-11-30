cb4b49a1a5d2d024b45fc2aa2a6786326769ee4b
00011763 <wiut11763@gmail.com>
2021-11-30 22:27:49 +0500

    added CW class and inited

---
+++ classes.py
@@ -0,0 +1,6 @@
+class CW:
+    def __init__(self, deadline, submissionTime):
+        self.deadline = deadline
+        self.submissionTime = submissionTime
+
+
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+from classes import CW
+from datetime import datetime
+
+csfCW = CW(
+    deadline=datetime(2020, 12, 1, 23, 59),
+    submissionTime=datetime(2020, 12, 2, 4, 59)
+)
+
+
