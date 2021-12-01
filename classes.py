7138d408bf0e379fec6c23ca27cd7eff1abb2f32
00011763 <wiut11763@gmail.com>
2021-12-01 11:03:23 +0500

    late submission logic done

--- classes.py
+++ classes.py
@@ -1,6 +1,49 @@
+from datetime import datetime, timedelta
+
+
 class CW:
-    def __init__(self, deadline, submissionTime):
+    ZERO_MARK = 0
+    FULL_MARK = 1
+    DECREASED_MARK = 2
+
+    marks = [
+        "Mark = 0",
+        "Full Mark",
+        "Minus 10 marks from overall marks but not below 40",
+    ]
+
+    def __init__(self, deadline: datetime, submission_time: datetime, has_valid_reason_ls, is_mc_ls_accepted):
         self.deadline = deadline
-        self.submissionTime = submissionTime
+        self.submissionTime = submission_time
+        self.has_valid_reason_ls = has_valid_reason_ls
+        self.is_mc_ls_accepted = is_mc_ls_accepted
 
+    def is_on_time(self):
+        submission_time = self.submissionTime
+        deadline = self.deadline
+        return submission_time < deadline
 
+    def is_within_24_hours(self):
+        submission_time = self.submissionTime
+        deadline = self.deadline
+        if self.is_on_time():
+            return 'invalid'
+        t: datetime = submission_time - deadline
+        return t < timedelta(days=1)
+
+    def get_full_mark_category(self):
+        if self.is_on_time():
+            return self.FULL_MARK
+
+        if self.is_within_24_hours():
+            if self.has_valid_reason_ls and self.is_mc_ls_accepted:
+                return self.FULL_MARK
+            else:
+                return self.DECREASED_MARK
+
+        return self.ZERO_MARK
+
+    def get_full_mark(self):
+        mark_category = self.get_full_mark_category()
+        print(mark_category)
+        return self.marks[mark_category]
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from classes import CW
 from datetime import datetime
 
-csfCW = CW(
-    deadline=datetime(2020, 12, 1, 23, 59),
-    submissionTime=datetime(2020, 12, 2, 4, 59)
+csf_cw = CW(
+    deadline=datetime(2020, 12, 3, 23, 59),
+    submission_time=datetime(2020, 12, 5, 4, 59)
 )
 
+print(csf_cw.is_on_time())
+print(csf_cw.is_within_24_hours())
+print(csf_cw.marks)
+print(csf_cw.get_full_mark())
 
